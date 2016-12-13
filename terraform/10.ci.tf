a2561bca58ce031c4d6897d87dfb10d91c9eaeef
neilmillard <neil@neilmillard.com>
2016-12-13 11:44:50 +0000

    correct syntax

--- terraform/10.ci.tf
+++ terraform/10.ci.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "CI" {
-  count = 1
-  ami = "ami-b73b63a0"
-  instance_type = "t2.medium"
-  subnet_id = "subnet-6cb33537"
-  security_groups = ["${aws_security_group.interviewUser1.id}"]
+  count             = 1
+  ami               = "ami-b73b63a0"
+  instance_type     = "t2.medium"
+  subnet_id         = "subnet-6cb33537"
+  security_groups   = ["${aws_security_group.interviewUser1.id}"]
   source_dest_check = false
-  user_data = "${file(\"cloud-config/ci.yml\")}"
-  tags = {
-    Name = "CI server"
+  user_data         = "${file("cloud-config/ci.yml")}"
+  tags              = {
+                    Name = "CI server"
   }
 }
